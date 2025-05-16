623675d8935939bfa2c1ffb33ed693cf64c2a12b
SyrаxX- -xx <gera8337@gmail.com>
2025-05-17 00:33:49 +0200

    Style changes

---
+++ HackathonWebsite/wwwroot/js/Snow.js
@@ -0,0 +1,23 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const snowContainer = document.createDocumentFragment();
+
+    for (let i = 0; i < 200; i++) {
+        const snowflake = document.createElement('div');
+        snowflake.classList.add('snow');
+
+        const size = Math.random() * 4 + 2;
+        const left = Math.random() * window.innerWidth;
+        const delay = Math.random() * 10;
+        const duration = Math.random() * 10 + 5;
+
+        snowflake.style.width = `${size}px`;
+        snowflake.style.height = `${size}px`;
+        snowflake.style.left = `${left}px`;
+        snowflake.style.animationDuration = `${duration}s`;
+        snowflake.style.animationDelay = `${delay}s`;
+
+        snowContainer.appendChild(snowflake);
+    }
+
+    document.body.appendChild(snowContainer);
+});
